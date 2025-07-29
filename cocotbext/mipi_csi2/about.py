e459375cbbe0b35ad861a646564121617e8242ed
David Stonely <david.stonely@oxos.com>
2025-07-29 13:45:17 -0400

    chore: bump version to 0.2.0

--- cocotbext/mipi_csi2/about.py
+++ cocotbext/mipi_csi2/about.py
@@ -2,7 +2,7 @@
 Version information for cocotbext-csi2
 """
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __author__ = "CSI-2 Extension Contributors"
 __email__ = "contributors@cocotbext-csi2.org"
 __license__ = "MIT"
